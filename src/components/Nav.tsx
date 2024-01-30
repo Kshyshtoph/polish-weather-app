70f0e366921c0bf92fbbf61320a177b5111f0845
Krzysztof Baum <baumkrzysztof@gmail.com>
2024-01-30 09:49:44 +0100

    fix nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,26 +1,13 @@
+import Link from "next/link";
+
 function Nav() {
   return (
     <nav className="bg-gray-800 text-white relative h-16 min-w-full  top-0 left-0 ">
       <ul className=" flex row container mx-auto gap-5 md:gap-10">
         <li>
-          <a href="/" className="block p-2">
-            Dashboard
-          </a>
-        </li>
-        <li>
-          <a href="#" className="block p-2">
-            Reports
-          </a>
-        </li>
-        <li>
-          <a href="#" className="block p-2">
-            Analytics
-          </a>
-        </li>
-        <li>
-          <a href="#" className="block p-2">
-            Settings
-          </a>
+          <Link href="/" className="block p-2">
+            Główna
+          </Link>
         </li>
       </ul>
     </nav>
